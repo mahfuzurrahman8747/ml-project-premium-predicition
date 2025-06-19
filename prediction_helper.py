6f2cf8b64837e95cd8d80a453f3ea225f51c2d61
Md Mahfuzur Rahman <mahfuzurrahman8747@gmail.com>
2025-06-20 00:19:13 +0600

    updated file path

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts/model_young.joblib")
-model_rest = joblib.load("artifacts/model_rest.joblib")
-scaler_young = joblib.load("artifacts/scaler_young.joblib")
-scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
+model_young = joblib.load("C:\\Users\Md Mahfuzur Rahman\OneDrive\Desktop\ml-project-premium-predicition\\artifacts\model_young.joblib")
+model_rest = joblib.load("C:\\Users\Md Mahfuzur Rahman\OneDrive\Desktop\ml-project-premium-predicition\\artifacts\model_rest.joblib")
+scaler_young = joblib.load("C:\\Users\Md Mahfuzur Rahman\OneDrive\Desktop\ml-project-premium-predicition\\artifacts\scaler_young.joblib")
+scaler_rest = joblib.load("C:\\Users\Md Mahfuzur Rahman\OneDrive\Desktop\ml-project-premium-predicition\\artifacts\scaler_rest.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
